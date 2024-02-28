d647d0bae8ce534f5fb35e671f12a735baa5d4af
code.polyglot <rahman.dev.niftaliyev@gmail.com>
2024-02-28 23:40:00 +0400

    navbar fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,20 +2,16 @@ import React from "react";
 import {
   Box,
   Flex,
-  Avatar,
-  HStack,
   IconButton,
-  Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
   useDisclosure,
   useColorModeValue,
+  Link as ChakraLink,
+  Collapse,
+  HStack,
   Stack,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import logo from "./../assets/logo.png";
 
@@ -45,78 +41,67 @@ const Links = [
 const NavBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  const style = {
-    padding: "5px",
-    border: "1px solid #383d47",
-    borderRadius: "5px",
-    display: isOpen ? "block" : "none",
-  };
-
   return (
-    <>
-      <Box
-        bgGradient={useColorModeValue(
-          "linear(to-t, #f1f1f1, #f1f1f1)",
-          "linear(to-t, #1a202c, #1a202c)"
-        )}
-        px={4}
-      >
-        <Flex alignItems={"center"} justifyContent={"space-between"}>
-          <HStack spacing={8} alignItems={"center"}>
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: isOpen && "flex", md: "flex" }}
-              flexDirection={{
-                base: isOpen ? "column" : "none",
+    <Box
+      bgGradient={useColorModeValue(
+        "linear(to-t, #f1f1f1, #f1f1f1)",
+        "linear(to-t, #1a202c, #1a202c)"
+      )}
+      px={4}
+    >
+      <Flex h={16} alignItems="center" justifyContent="space-between">
+        <IconButton
+          size="md"
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          aria-label="Open Menu"
+          display={{ md: "none" }}
+          onClick={isOpen ? onClose : onOpen}
+        />
+        <HStack as="nav" spacing={4} display={{ base: "none", md: "flex" }}>
+          {Links.map((link) => (
+            <ChakraLink
+              key={link.id}
+              as={Link}
+              to={link.url}
+              p={2}
+              color="black"
+              _hover={{
+                textDecoration: "none",
+                bg: useColorModeValue("gray.200", "gray.700"),
+                borderRadius: "md",
               }}
             >
-              <IconButton
-                size={"md"}
-                icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                aria-label={"Open Menu"}
-                display={{ md: "none" }}
-                onClick={isOpen ? onClose : onOpen}
-              />
-              {isOpen ? (
-                <Box
-                  position={isOpen ? "absolute" : "relative"}
-                  left={isOpen ? "0" : "0"}
-                  zIndex={isOpen ? "100" : "0"}
-                  p={4}
-                  borderRadius={isOpen ? "0" : "10px"}
-                  w={isOpen ? "50%" : "auto"}
-                  h={isOpen ? "100%" : "auto"}
-                  top={isOpen ? "30" : "0"}
-                >
-                  {Links.map((link, index) => (
-                    <span key={index} style={style}>
-                      <Link key={link.id} to={link.url}>
-                        {link.title}
-                      </Link>
-                    </span>
-                  ))}
-                </Box>
-              ) : (
-                <>
-                  {Links.map((link, index) => (
-                    <span key={index} style={style}>
-                      <Link key={link.id} to={link.url}>
-                        {link.title}
-                      </Link>
-                    </span>
-                  ))}
-                </>
-              )}
-            </HStack>
-            <Box>
-              <img src={logo} alt="logo" width="60" />
-            </Box>
-            {/*  */}
-          </HStack>
+              {link.title}
+            </ChakraLink>
+          ))}
+        </HStack>
+        <Flex alignItems="center">
+          <img src={logo} alt="logo" width="60" />
         </Flex>
-      </Box>
-    </>
+      </Flex>
+      <Collapse in={isOpen} animateOpacity>
+        <Box p={4}>
+          <Stack spacing={4}>
+            {Links.map((link) => (
+              <ChakraLink
+                key={link.id}
+                as={Link}
+                to={link.url}
+                p={2}
+                color="black"
+                _hover={{
+                  textDecoration: "none",
+                  bg: useColorModeValue("gray.200", "gray.700"),
+                  borderRadius: "md",
+                }}
+              >
+                {link.title}
+              </ChakraLink>
+            ))}
+          </Stack>
+        </Box>
+      </Collapse>
+    </Box>
   );
 };
 
